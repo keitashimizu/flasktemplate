03391368258144292310678801c17d4fe8c6414a
ktshmz_test+1-at-977453326449 <ktshmz@nttpc.co.jp>
2021-12-22 13:49:26 +0900

    modified app root

--- api/__init__.py
+++ api/__init__.py
@@ -1,19 +1,2 @@
-from flask import Flask, make_response, jsonify
-from .views.user import user_router
-from flask_cors import CORS
-from api.database import db
-import config
 
 
-def create_app():
-
-  app = Flask(__name__)
-  CORS(app)
-  app.config.from_object('config.Config') # configの追加
-  db.init_app(app) #configを使ってDB接続初期化　
-
-  app.register_blueprint(user_router, url_prefix='/api') #userブループリントの追加
-
-  return app
-
-app = create_app()
--- app.py
+++ app.py
@@ -1,3 +1,20 @@
-from api import app
+#from api import app
+from flask import Flask, make_response, jsonify
+from api.views.user import user_router
+from flask_cors import CORS
+from api.database import db
+import config
+
+def create_app():
+
+  app = Flask(__name__)
+  CORS(app)
+  app.config.from_object('config.Config') # configの追加
+  db.init_app(app) #configを使ってDB接続初期化　
+  app.register_blueprint(user_router, url_prefix='/api') #userブループリントの追加
+
+  return app
+
 if __name__=='__main__':
+    app = create_app()
     app.run()
